2c5d68ccc2f38c79884e7cc75e2b6f98d8948671
vsingh18567 <59497793+vsingh18567@users.noreply.github.com>
2021-06-27 15:50:23 +0800

    working file splitter

--- pgn-split.py
+++ pgn-split.py
@@ -1,48 +1,86 @@
 from typing import TextIO
-import chess.pgn
+from chess import pgn, Move
 import io
-def get_game(f: TextIO) -> str :
-    '''
-    reads lines of a file till it has generated an entire game
-    '''
-    starting_character : str = "["
-    game : str = ""
-    line : str = None
-    while starting_character != "1":
-        line = f.readline()
-        try:
-            starting_character = line[0]
-        except:
-            return "EOF"
-    game += line
-    end = False
-    if "*" in line:
-        end = True
-    while not end:
-        line = f.readline()
+import copy
+import os
+
+
+class PgnSplitter:
+    
+    def __init__(self, filepath):
+        self.counter = 0
+        self.filepath = filepath
+        self.get_games(filepath)
+
+    def get_game(self, f: TextIO) -> str :
+        '''
+        reads lines of a file till it has generated an entire game
+        '''
+        starting_character : str = "["
+        game : str = ""
+        line : str = None
+        while starting_character != "1":
+            line = f.readline()
+            try:
+                starting_character = line[0]
+            except:
+                return "EOF"
         game += line
+        end = False
         if "*" in line:
             end = True
-    return game
+        while not end:
+            line = f.readline()
+            game += line
+            if "*" in line:
+                end = True
+        return game
 
 
-def get_games(filepath: str):
-    with open(filepath, 'r') as f:
-        a = None
-        while a != "EOF":
-            a = get_game(f)
-            game = chess.pgn.read_game(io.StringIO(a))
-            
-
-def parse_game(game : chess.pgn.Game):
-    variations = 
+    def get_games(self, filepath: str):
+        with open(filepath, 'r') as f:
+            a = None
+            while a != "EOF":
+                a = self.get_game(f)
+                game = pgn.read_game(io.StringIO(a))
+                self.parse_game(game)
 
 
+    def parse_game(self, game: pgn.GameNode):
+        vars = game.variations
+        if game.next() == None:
+            self.remove_variations(game.game())
+            self.write_game(str(game.game()))
+        elif len(vars) == 1:
+            self.parse_game(game.next())
+        else:
+            for i in range(len(vars)):
+                new_game = copy.deepcopy(game)
+                new_game.promote_to_main(new_game.variations[i])
+                self.parse_game(new_game.next())
 
-def func (filepath: str):
-    get_games(filepath=filepath)
+    def remove_variations(self, game: pgn.GameNode):
+            vars = game.variations
+            if len(vars) == 0:
+                self.write_game(str(game.game()))
+            elif len(vars) == 1:
+                self.remove_variations(game.next())
+            else:
+                for var in vars[1:]:
+                    game.remove_variation(var)
+                self.remove_variations(game.next())            
+        
+
+    def write_game(self, string):
+        filepath = f"pgns/game{self.counter}.pgn"
+        file_exists = os.path.isfile(filepath)
+        if file_exists:
+            os.remove(filepath)
+        with open (filepath, "w") as f:
+            f.write(string)
+        self.counter += 1
 
 
-func('lichess2.pgn')
+PgnSplitter('lichess2.pgn')
 
 
